da67784acc7ca7a2b8cfba180dda2eb523472747
XuanTu <nguyenxuantu306203@gmail.com>
2023-10-05 23:01:13 +0700

    XuanTu_FE
    Commit phần giao diện header, index trang chủ, footer, login, register, product_detail >.<

--- src/main/java/com/greenfarm/controller/HomeControllerTest.java
+++ src/main/java/com/greenfarm/controller/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package com.poly.controller;
+package com.greenfarm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 
-public class HomeController {
+public class HomeControllerTest {
     public String Home(Model model){
         return "index";
     }
 
-
+    @RequestMapping("/product-detail")
+    public String ProductDetail(Model model){
+        return "product/detail";
+    }
 
     @RequestMapping("/login")
     public String Login(Model model){
@@ -24,5 +27,7 @@ public class HomeController {
     public String Register(Model model){
         return "register";
     }
+
+    
 
 }
