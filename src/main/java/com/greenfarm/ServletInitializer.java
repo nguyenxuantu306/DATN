8558f5db91a0de56941255d676a7ec82a719976b
XuanTu <nguyenxuantu306203@gmail.com>
2023-10-09 09:48:52 +0700

    FE_Admin
    addProduct, addTour

--- src/main/java/com/greenfarm/ServletInitializer.java
+++ src/main/java/com/greenfarm/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.poly;
+package com.greenfarm;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
