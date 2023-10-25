43f6b8943a3c8e415e3060428aaddf7e200eb991
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-10-25 15:51:03 +0700

    .

--- src/main/java/com/greenfarm/Controller/SecurityController.java
+++ src/main/java/com/greenfarm/Controller/SecurityController.java
@@ -1,7 +1,5 @@
 package com.greenfarm.Controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,13 +7,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.greenfarm.dao.UserDAO;
 import com.greenfarm.entity.User;
 import com.greenfarm.service.UserService;
-import com.greenfarm.service.impl.UserServerImpl;
 
 @Controller
 public class SecurityController {
