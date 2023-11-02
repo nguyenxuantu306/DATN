363a03f1dbdabb845bc5c63ba036ad434d23fcca
HoangTrongNghia <nghiahoang242@gmail.com>
2023-11-02 14:03:15 +0700

    ++

--- src/main/java/com/greenfarm/DAO/CommentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.greenfarm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greenfarm.entity.Comment;
-
-public interface CommentDAO extends JpaRepository<Comment, Integer> {
-
-}
--- src/main/java/com/greenfarm/DAO/RoleDAO.java
+++
@@ -1,9 +0,0 @@
-package com.greenfarm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greenfarm.entity.UserRole;
-
-public interface RoleDAO extends JpaRepository<UserRole, Integer> {
-
-}
