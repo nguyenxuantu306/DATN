7ebe0ae03b7022bb7ec340d1d1cbfefb43c79758
Phongbtti1 <vothanhphong2000bt@gmail.com>
2023-11-07 21:31:34 +0700

    update

---
+++ src/main/java/com/greenfarm/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package com.greenfarm.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.greenfarm.entity.User;
+
+public interface UserDAO extends JpaRepository<User, Integer> {
+
+	@Query("SELECT DISTINCT ur.user FROM UserRole ur WHERE ur.role.id IN (1,2)")
+	List<User> getAdministrators();
+
+	Optional<User> findByEmail(String email);
+
+}
