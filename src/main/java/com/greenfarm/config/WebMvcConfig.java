4b2848cd18e1bcd4920d1b6d7a4945d2c5751e33
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-11-12 11:54:57 +0700

    language

---
+++ src/main/java/com/greenfarm/config/WebMvcConfig.java
@@ -0,0 +1,46 @@
+package com.greenfarm.config;
+
+import java.time.Duration;
+import java.util.Locale;
+
+import org.hibernate.validator.spi.messageinterpolation.LocaleResolver;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieDomain("myAppLocaleCookie");
+        // resolver.setDefaultLocale(Locale.ENGLISH);
+
+        Duration maxAge = Duration.ofSeconds(3600);
+        resolver.setCookieMaxAge(maxAge);
+        return null;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("language");
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+    }
+
+}
