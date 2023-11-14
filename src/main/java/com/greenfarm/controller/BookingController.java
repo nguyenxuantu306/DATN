00b1b403755b597f391b7a6c37410dbf9a921a87
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-11-14 21:37:07 +0700

    update

--- src/main/java/com/greenfarm/controller/BookingController.java
+++ src/main/java/com/greenfarm/controller/BookingController.java
@@ -1,50 +1,31 @@
 package com.greenfarm.controller;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.greenfarm.dto.BookingDTO;
-import com.greenfarm.dto.OrderDTO;
-import com.greenfarm.dto.ProductDTO;
-import com.greenfarm.dto.TourDTO;
 import com.greenfarm.entity.Booking;
-import com.greenfarm.entity.Order;
-import com.greenfarm.entity.OrderDetail;
 import com.greenfarm.entity.PaymentMethod;
-import com.greenfarm.entity.Product;
 import com.greenfarm.entity.StatusBooking;
 import com.greenfarm.entity.Tour;
 import com.greenfarm.entity.User;
 import com.greenfarm.service.BookingService;
-import com.greenfarm.service.OrderService;
 import com.greenfarm.service.StatusBookingService;
 import com.greenfarm.service.TourService;
 import com.greenfarm.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 
 @Controller
 public class BookingController {
@@ -103,7 +84,7 @@ public class BookingController {
 		booking.setPaymentmethod(paymentMethod);
 
 		bookingService.saveBooking(booking);
-		return "redirect:/booking/success";
+		return "redirect:/success";
 	}
 	
 
