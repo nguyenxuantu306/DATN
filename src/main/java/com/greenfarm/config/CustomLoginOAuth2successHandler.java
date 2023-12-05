60d4765ffd8556de4417337724575954ce28a79f
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-12-05 12:03:19 +0700

    insert

---
+++ src/main/java/com/greenfarm/config/CustomLoginOAuth2successHandler.java
@@ -0,0 +1,106 @@
+package com.greenfarm.config;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.greenfarm.dto.Provider;
+import com.greenfarm.entity.User;
+import com.greenfarm.service.UserService;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomLoginOAuth2successHandler implements AuthenticationSuccessHandler {
+	
+	
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	UserDetailsService details;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		 if (authentication != null) {
+		        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+		        String provider = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+		        // provider là tên đăng ký của nhà cung cấp OAuth2 (ví dụ: "google", "facebook")
+		      //  return "Logged in via " + provider ;
+		        String email = oAuth2User.getAttribute("email");
+		 	   try {
+		 		  User dangnhap = userService.findByEmail(email);
+		 		  if (dangnhap != null) {
+		 			// Lấy thông tin đăng nhập từ người dùng
+		    			//String username = dangnhap.getEmail();  // Giả sử email là tên người dùng
+
+		    			setAuthenticationInSecurityContext(dangnhap.getEmail());
+		    			// Tải thông tin người dùng từ UserDetailsService
+//		    			UserDetails userDetails = details.loadUserByUsername(username);
+//
+//		    			// Tạo một đối tượng Authentication
+//		    			Authentication authentication2 = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//
+//		    			// Đặt thông tin xác thực vào SecurityContext
+//		    			SecurityContext sc = SecurityContextHolder.getContext();
+//		    			sc.setAuthentication(authentication2);
+//		    			SecurityContext check = SecurityContextHolder.getContext();
+		    			
+		    			response.sendRedirect("/");
+				}else {
+					response.sendRedirect("/register/completelogin");
+				}
+		 		  
+		 		  
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				response.sendRedirect("/logoff");
+				
+			}
+		       
+		     
+	}else {
+		System.out.println("chưa đăng nhập");
+		response.sendRedirect("/login");
+	}
+		 
+		 
+	
+
+}
+	
+	private void setAuthenticationInSecurityContext(String username) {
+	    // Tải thông tin người dùng từ UserDetailsService
+	    UserDetails userDetails = details.loadUserByUsername(username);
+
+	    // Tạo một đối tượng Authentication
+	    Authentication authentication2 = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+	    // Đặt thông tin xác thực vào SecurityContext
+	    SecurityContext sc = SecurityContextHolder.getContext();
+	    sc.setAuthentication(authentication2);
+	}
+	}
---
+++ src/main/java/com/greenfarm/controller/CameraController.java
@@ -0,0 +1,132 @@
+package com.greenfarm.controller;
+
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.sarxos.webcam.Webcam;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.BitArray;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.greenfarm.service.impl.WebcamService;
+
+@RestController
+@RequestMapping("/api/camera")
+@CrossOrigin(origins = "http://localhost:8080/")
+public class CameraController {
+    @Autowired
+    private WebcamService webcamService;
+
+//    @GetMapping("/capture")
+//    public ResponseEntity<byte[]> capture() {
+//        BufferedImage bufferedImage = webcamService.capture();
+//
+//        // Convert BufferedImage to byte array
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        try {
+//            ImageIO.write(bufferedImage, "png", baos);
+//            baos.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Set the content type as image/png
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.IMAGE_PNG);
+//        
+//
+//        // Return the byte array as ResponseEntity
+//        return new ResponseEntity<>(baos.toByteArray(), headers, HttpStatus.OK);
+//    }
+    
+    @GetMapping("/soatve")
+    public String soatve() {
+    	
+    	return null;
+    }
+    
+    @GetMapping("/captureqr")
+    public String capture1() {
+    	
+    	// Lấy danh sách các camera
+    	List<Webcam> webcams = Webcam.getWebcams();
+System.out.println(webcams);
+    	// Chọn camera đầu tiên trong danh sách
+    	Webcam selectedWebcam = webcams.get(0);
+    	System.out.println(selectedWebcam);
+    	
+    	// Đóng camera hiện tại
+    	
+    	//webcam.close();
+
+    	// Mở camera mới
+    	selectedWebcam.open(true);
+
+    	// Đổi sang camera khác (ví dụ: camera thứ hai)
+    	//selectedWebcam = webcams.get(1);
+
+        try {
+            //BufferedImage bufferedImage = webcamService.capture();
+            BufferedImage bufferedImage = selectedWebcam.getImage();
+
+            // Convert BufferedImage to byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", baos);
+            baos.flush();
+
+            // Set the content type as image/png
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+
+            // Return the byte array as ResponseEntity
+           
+
+//            // Decode QR code from the captured image
+//            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(
+//                    new BufferedImageLuminanceSource(bufferedImage)));
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer( 
+            		new BufferedImageLuminanceSource(bufferedImage)
+            		));
+            
+            
+            Result result = new MultiFormatReader().decode(bitmap);
+
+            // You can use the result.getText() to get the decoded QR code value
+            String qrCodeValue = result.getText();
+
+            // Perform any action with the decoded QR code value
+            System.out.println("Decoded QR Code: " + qrCodeValue);
+
+            // You can return the decoded QR code value as part of the response
+            ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(baos.toByteArray(), headers, HttpStatus.OK);
+            return "redirect:/"+qrCodeValue;
+           // return ResponseEntity.ok(qrCodeValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error capturing and decoding QR code.");
+            System.out.println("loi");
+            return "/captureqr"; 
+        }
+    }
+}
