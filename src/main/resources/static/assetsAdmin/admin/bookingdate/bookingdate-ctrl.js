3d36d3284dba6c39092ed8c5154d1b10492245c4
HungNVCODE-GITHUB-2003 <hungcode2003@gmail.com>
2023-12-06 18:53:11 +0700

    Updarte

--- src/main/resources/static/assetsAdmin/admin/bookingdate/bookingdate-ctrl.js
+++ src/main/resources/static/assetsAdmin/admin/bookingdate/bookingdate-ctrl.js
@@ -29,7 +29,7 @@ app.controller("bookingdate-ctrl", function($scope, $http) {
 	}
 	//Tính tổng
 	$scope.calculateTotal = function(item) {
-		return (item.adultticketnumber * item.tour.pricings.adultprice) + (item.childticketnumber * item.tour.pricings.childprice);
+		return (item.booking.adultticketnumber * item.booking.tour.pricings.adultprice) + (item.booking.childticketnumber * item.booking.tour.pricings.childprice);
 	}
 
 
