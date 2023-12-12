31e09a091a31baa1f77944b03c5cbba5a9c4fcdf
HungNVCODE-GITHUB-2003 <hungcode2003@gmail.com>
2023-12-12 23:50:47 +0700

    Updaterating

--- src/main/java/com/greenfarm/dao/ReviewDao.java
+++ src/main/java/com/greenfarm/dao/ReviewDao.java
@@ -1,6 +1,7 @@
 package com.greenfarm.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 import com.greenfarm.entity.Product;
 import com.greenfarm.entity.ReportRevenue;
 import com.greenfarm.entity.Review;
+import com.greenfarm.entity.StarCount;
 import com.greenfarm.entity.User;
 
 
@@ -27,4 +29,10 @@ public interface ReviewDao extends JpaRepository<Review, Integer> {
 	
 	@Query("SELECT r FROM Review r WHERE r.user = :user AND r.product = :product")
     Review findReviewByUserAndProduct(@Param("user") User user, @Param("product") Product product);
+
+	@Query("SELECT new StarCount(r.rating, COUNT(r.rating)) FROM Review r WHERE r.product.productid = :productId GROUP BY r.rating")
+	List<StarCount> countReviewsByRating(Integer productId);
+
+
+
 }
