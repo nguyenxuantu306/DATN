e9005b8b574cd086a971fd48c1a54ce4141ef0b0
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-12-13 11:25:50 +0700

    change

--- src/main/java/com/greenfarm/entity/Booking.java
+++ src/main/java/com/greenfarm/entity/Booking.java
@@ -3,9 +3,6 @@ package com.greenfarm.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
