96cb8e77a14e1f2404145bae084ad1e16373974c
Xuân Tú <93813613+nguyenxuantu306@users.noreply.github.com>
2023-12-18 15:43:51 +0700

    remove file

--- src/main/java/com/greenfarm/GreenfarmApplication.java
+++ src/main/java/com/greenfarm/GreenfarmApplication.java
@@ -3,14 +3,20 @@ package com.greenfarm;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class GreenfarmApplication {
+public class GreenfarmApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(GreenfarmApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GreenfarmApplication.class);
+	}
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
